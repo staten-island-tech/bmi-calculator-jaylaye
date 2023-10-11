954deaff276e3af57ec781811168cd1bb13c1173
jaylaye <yejayla69@gmail.com>
2023-10-11 09:28:59 -0400

    bmi

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const username = prompt("what is your name");
-const height = prompt("what is your height in cm");
-const weight = prompt("what is your weight in kg");
+
+let username = prompt("what is your name");
+let height = prompt("what is your height in cm");
+let weight = prompt("what is your weight in kg");
 
 function calcBMI(height, weight){
     return(weight / height ** 2) *10000
+}
+const bmi = calcBMI();
+console.log ("bmi");
 
+function rangeBMI(bmi){
     if(bmi <= 18.5){
         return "ur underweight go eat!";
     }
+    else if(bmi >=18.5 && bmi <=24.99999){
+        return "wow ur so healthy!";
+    }
+    else if(bmi <=29.99999 && bmi >=25){
+        return "u a bit big! just a bit!";
+    }
     else if(bmi >=30){
-        return "ur obese go on a diet!"
+        return "ur obese go on a diet!";
     }
 }
 BMI = calcBMI
-console.log ("bmi")
+
